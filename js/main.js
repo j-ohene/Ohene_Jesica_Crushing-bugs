c693f89151f8a667edbcf112144c90470a5f8af3
j-ohene <jesicaohene1@gmail.com>
2023-06-26 23:04:59 -0400

    plan for reset

--- js/main.js
+++ js/main.js
@@ -3,8 +3,14 @@ const theButtons = document.querySelectorAll("#buttonHolder img"),
     puzzleBoard = document.querySelector(".puzzle-board"),
     puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
     dropZones = document.querySelectorAll(".drop-zone");
+   
     //since there are no parameters or js  set for #resetBut, the variable will be set here
-
+  //bug fix 2 will go here
+    //will use a forEach loop and if statement
+    //i want to loop through dropzones and check if there is?
+    //check with firstChild
+    //if there is a child
+    //puzzlePiecesDiv.appendChild(something needs to go here)
 
 
 //store the dragged piece in a global variable
@@ -35,6 +41,12 @@ function handleDrop(e) {
     //this line moves the dragged piece from the left side of the board
     //into whatever dropzone we choose.
     this.appendChild(draggedPiece);
+
+        //bug fix 1 will go here
+    //can also check if there are no children
+    if(this.puzzlePieces >=1)  {
+        return;
+    }
 }
 
 //function resetBut(e){}
